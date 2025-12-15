57e538fe0f7cf470b9f220f5531ab22fbcdcbcaf
Utkarsh Kumar <152621458+utkarshkgithub@users.noreply.github.com>
2025-12-15 12:30:50 +0530

    feat: cors resolve chrome web

--- winkit/settings.py
+++ winkit/settings.py
@@ -172,13 +172,7 @@ SIMPLE_JWT = {
 }
 
 # CORS Configuration
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:8000",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:8000",
-    "https://winkit.onrender.com"
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
